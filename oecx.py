cc0a8f3926936a77bfe4ce1458b4a0ac64e05af9
FranciscoRLanza <frjrodriguezla@unal.edu.co>
2021-02-24 14:39:41 -0500

    Initial commit

---
+++ oecx.py
@@ -0,0 +1,43 @@
+# Librerias requeridas para el modulo:
+import requests
+import networkx as nx
+import json
+import csv
+
+# dicionario de paises
+with open("country_names.tsv") as tsvfile:
+    tsvreader = csv.reader(tsvfile, delimiter='\t')
+    next(tsvreader)
+    countries=[]
+    for line in tsvreader:
+        country={}
+        country['id']= line[0]
+        country['id_3char']= line[1]
+        country['name']= line[2]
+        countries.append(country)
+
+
+def net(hs4,year,traide_flow):
+    # select impor or export
+    if traide_flow == 'export':
+        url_b= 'https://oec.world/olap-proxy/data?cube=trade_i_baci_a_92&drilldowns=Year,Importer%20Country&measures=Trade%20Value&parents=true&Year={}&Exporter%20Country={}&HS4={}&properties=Importer%20Country%20ISO%203'
+    if traide_flow == 'import':
+        url_b= 'https://oec.world/olap-proxy/data?cube=trade_i_baci_a_92&drilldowns=Year,Exporter%20Country&measures=Trade%20Value&parents=true&Year={}&Importer%20Country={}&HS4={}&properties=Exporter%20Country%20ISO%203'
+
+    dates=[]
+    for country in countries:
+        ID=country['id']
+        url=url_b.format(year,ID,hs4)
+        cont=requests.get(url).content.decode("utf-8")
+        dic = json.loads(cont)['data']
+        for md in dic:
+            md.update(country)
+        dates += dic
+
+    #networkx
+    edges=[[key['name'],key['Country'] ] for key in dates ]
+    G = nx.DiGraph()
+    G.add_edges_from(edges)
+
+
+    return G
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'oecx',
+  packages = ['oecx'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Network builder for the Observatory for Economic Complexity ',
+  author = 'Francisco R. Lanza',
+  author_email = 'frjrodriguezla@unal.edu.co',
+  url = 'https://github.com/{user_name}/{repo}', # use the URL to the github repo
+  download_url = 'https://github.com/{user_name}/{repo}/tarball/0.1',
+  keywords = ['oec', 'networkx', 'networks'],
+  classifiers = [],
+)
